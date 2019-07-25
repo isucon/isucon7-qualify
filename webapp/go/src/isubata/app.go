c7cc40c2bdad9a0f93fcc5241b366ebfda046efb
koiizuka <koiizuka@ip-10-3-11-225.ap-northeast-1.compute.internal>
2019-07-25 19:33:45 +0900

    fix query

--- webapp/go/src/isubata/app.go
+++ webapp/go/src/isubata/app.go
@@ -480,7 +480,7 @@ func myGetMessage(c echo.Context) error {
 		_, err := db.Exec("INSERT INTO haveread (user_id, channel_id, message_id, updated_at, created_at)"+
 			" VALUES (?, ?, ?, NOW(), NOW())"+
 			" ON DUPLICATE KEY UPDATE message_id = ?, updated_at = NOW()",
-			userID, chanID, response[mxIdx]["id"], response[mxIdx]["id"])
+			userID, chanID, response[0]["id"], response[0]["id"])
 		if err != nil {
 			return err
 		}
