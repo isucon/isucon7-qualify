e7c9d3947bdccdc236328d579b3f3b7b94836270
YamagishiRyu <bubusu.ryu@gmail.com>
2020-08-10 16:39:00 +0900

    :zap: Update: data source name

--- webapp/go/batch/image_dump.go
+++ webapp/go/batch/image_dump.go
@@ -17,8 +17,8 @@ import (
 )
 
 func init_db() (*sqlx.DB, error) {
-	dsn := fmt.Sprintf("%s:@tcp(%s:%s)/isubata?parseTime=true&loc=Local&charset=utf8mb4",
-		"root", "localhost", "3306")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/isubata?parseTime=true&loc=Local&charset=utf8mb4",
+		"isucon", "isucon", "localhost", "3306")
 
 	log.Printf("Connecting to db: %q", dsn)
 	db, err := sqlx.Connect("mysql", dsn)
