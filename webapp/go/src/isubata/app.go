6b177d5a6e12b21678c543ae6105da56fe5256ee
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2021-05-03 00:37:18 +0900

    import pprof

--- webapp/go/src/isubata/app.go
+++ webapp/go/src/isubata/app.go
@@ -12,6 +12,7 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"net/http/pprof"
 	"os"
 	"strconv"
 	"strings"
@@ -721,6 +722,10 @@ func tRange(a, b int64) []int64 {
 }
 
 func main() {
+	go func() {
+		log.Println(http.ListenAndServe("127.0.0.1", nil))
+	}()
+
 	e := echo.New()
 	funcs := template.FuncMap{
 		"add":    tAdd,
